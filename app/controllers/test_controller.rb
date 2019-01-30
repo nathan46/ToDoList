f0162030ebe7ae9887e59f7d8f00ce7c656d5ba7
Nathan Auger <nathan.og17@gmail.com>
2019-01-30 18:53:38 +0100

    requete bdd OK

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -2,12 +2,4 @@ require 'pg'
 
 class TestController < ApplicationController
 
-  attr_accessor :db
-
-  def initialize()
-    db = PG::Connection.new('ec2-23-21-171-25.compute-1.amazonaws.com','5432',nil,nil,'d7snv8turfs4ff','ohyxckuotpgwbl','ab939bfbfdd30e5a78e55a3250ee2dc07bc3c5aa05362939b1300c31a0be206a')
-  end
-
-
-
 end
--- app/helpers/test_helper.rb
+++ app/helpers/test_helper.rb
@@ -1,12 +1,16 @@
+require 'pg'
+
 module TestHelper
 
-  def listAll db
+  def listAll
+    db = PG::Connection.new('ec2-23-21-171-25.compute-1.amazonaws.com','5432',nil,nil,'d7snv8turfs4ff','ohyxckuotpgwbl','ab939bfbfdd30e5a78e55a3250ee2dc07bc3c5aa05362939b1300c31a0be206a')
     liste = db.exec("SELECT * FROM liste")
     html = String.new
 
     liste.each do |line|
-      html += "#{line['text']}        "
+      html += "<h2> #{line['text']} </h2>"
     end
+    html.html_safe
 
   end
 
