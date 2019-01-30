973d1c52e1855611c956fc2e6a99e1cd5271590f
Nathan Auger <nathan.og17@gmail.com>
2019-01-30 21:30:58 +0100

    JSON OK

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'welcome#index'
-
-  get 'test' => 'test#home'
+  #root 'welcome#index'
+    get '/api/all' => 'api#all'
+    get '/api/one'  => 'api#one'
+    get '/api/two'  => 'api#two'
 end
